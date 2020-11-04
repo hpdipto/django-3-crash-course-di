9f24cef5f43809cb698e37a982c0505ce9c11a66
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-11-04 20:23:07 +0600

    Models updated

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -11,22 +11,38 @@ class Customer(models.Model):
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 
+class Tag(models.Model):
+
+	def __str__(self):
+		return self.name
+
+	name = models.CharField(max_length=200, null=True)
+
+
+
 class Product(models.Model):
+
+	def __str__(self):
+		return self.name
+
 	CATEGORY = (('Indoor', 'Indoor'),
 							('Outdoor', 'Outdoor'))
 
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True)
+	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
+
 
 
 class Order(models.Model):
+
 	STATUS = (('Pending', 'Pending'),
 						('Out for delivery', 'Out for delivery'),
 						('Delivered', 'Delivered'))
-	# customer = 
-	# product = 
+	customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
